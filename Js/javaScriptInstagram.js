9356e6fcf9c7693f78a1333fa22bb77501fa3ae8
Eyleen Pino <ey.pino@duocuc.cl>
2023-05-15 18:40:08 -0400

    detalles api instagram

--- Js/javaScriptInstagram.js
+++ Js/javaScriptInstagram.js
@@ -1,5 +1,74 @@
-$(function(){
-    const token = '';
+$(function() {
+    const token = 'IGQVJXa2xyeWlWa1pUQm9OQjdtMVVDMFEtUFgxeGNQOHRZAekNyaVJsQVJDdUZANYWx6Ym5KN0E0Y1ZATNWZAOSVVvRnJvbVRaNDBfMWllTnBLQW8xSTliU09GNHJTNWxvaHpuNi1OM1JmVXFMM0pXRDRnWAZDZD';
+    const url = `https://graph.instagram.com/me/media?fields=media_url,media_type,caption,permalink&refresh_access_token?grant_type=ig_refresh_token&&access_token=${token}`;
+  
+    $.get(url).then(function(response) {
+      let datosJson = response.data;
+      let contenido = '<div class="row" style="padding/left:5px">';
+      let currentIndex = 0;
+  
+      for (let p = 0; p < 3; p++) {
+        let feed = datosJson[p];
+        let titulo = feed.caption !== null ? feed.caption : '';
+        let tipo = feed.media_type;
+  
+        if (tipo === 'VIDEO') {
+          contenido += '<div class="col-12 col-sm-6 col-md-4"><video style="width: 26rem;height: 28rem; transition: 2s;" controls><source src="' + feed.media_url + '" type="video/mp4"><p style="position: absolute; color:white; align-self: center; width:26rem;">' + titulo + '</p></video></div>';
+        } else if (tipo === 'IMAGE') {
+          contenido += '<div class="col-12 col-sm-6 col-md-4"><img style="width: 26rem;height: 28rem; transition: 2s;" title="' + titulo + '" src="' + feed.media_url + '" onclick="window.open(\'' + feed.permalink + '\');"><p style="position: absolute; color:white; align-self: center; width:26rem;">' + titulo + '</p></div>';
+        }
+  
+        if (p === currentIndex) {
+          currentIndex = p;
+        }
+      }
+  
+      contenido += '</div>';
+      $('#insta').html(contenido);
+  
+      //chevron next
+      $('#next').click(function() {
+        if (currentIndex < datosJson.length - 1) {
+          currentIndex++;
+        } else {
+          currentIndex = 0;
+        }
+        actualizarFeed();
+      });
+  
+      //chevron prev
+      $('#prev').click(function() {
+        if (currentIndex > 0) {
+          currentIndex--;
+        } else {
+          currentIndex = datosJson.length - 1;
+        }
+        actualizarFeed();
+      });
+  
+      function actualizarFeed() {
+
+        let contenido = '<div class="row" style="padding/left:5px">';
+        for (let p = currentIndex; p < currentIndex + 3; p++) {
+          let feed = datosJson[p % datosJson.length];
+          let titulo = feed.caption !== undefined ? feed.caption : '';
+          let tipo = feed.media_type;
+  
+          if (tipo === 'VIDEO') {
+            contenido += '<div class="col-12 col-sm-6 col-md-4"><video style="width: 26rem;height: 28rem;" controls><source src="' + feed.media_url + '" type="video/mp4"><p style="position: absolute; color:white; align-self: center; width:26rem;">' + titulo + '</p></video></div>';
+          } else if (tipo === 'IMAGE') {
+            contenido += '<div class="col-12 col-sm-6 col-md-4"><img style="width: 26rem;height: 28rem;" title="' + titulo + '" src="' + feed.media_url + '" onclick="window.open(\'' + feed.permalink + '\'); loading="lazy"><p style="position: absolute; color:white; align-self: center; width:26rem;">' + titulo + '</p></div>';
+          }
+        }
+        contenido += '</div>';
+        $('#insta').html(contenido);
+      }
+    });
+  });
+
+
+/*$(function(){
+    const token = 'IGQVJXa2xyeWlWa1pUQm9OQjdtMVVDMFEtUFgxeGNQOHRZAekNyaVJsQVJDdUZANYWx6Ym5KN0E0Y1ZATNWZAOSVVvRnJvbVRaNDBfMWllTnBLQW8xSTliU09GNHJTNWxvaHpuNi1OM1JmVXFMM0pXRDRnWAZDZD';
     const url = `https://graph.instagram.com/me/media?fields=media_url,media_type,caption,permalink&refresh_access_token?grant_type=ig_refresh_token&&access_token=${token}`; 
 
     $.get(url).then(function(response){
@@ -7,21 +76,21 @@ $(function(){
         let datosJson=response.data
         let contenido= '<div class="row" style="padding/left:5px">';
 
-        for (let p=0; p< datosJson.length;p++){
+        for (let p=0; p<3;p++){
             let feed=datosJson[p];
             let titulo=feed.caption !== null ? feed.caption : '';
             let tipo = feed.media_type;
             if (tipo === 'VIDEO'){
-                contenido+='<div class="col-12 col-sm-6 col-md-4 col-lg-4 col-xl-4 col-xxl-4 image-overflow"><video style="width: 28rem;height: 30rem;" controls><source src="'+feed.media_url+'" type="video/mp4"></video></div>';  
+                contenido+='<div class="col-12 col-sm-6 col-md-4 col-lg-4 col-xl-4 col-xxl-4 image-overflow"><video style="width: 26rem;height: 28rem;" controls><source src="'+feed.media_url+'" type="video/mp4"><div class="opacity-hover"></div></video></div>';  
             }
             else if (tipo === 'IMAGE'){
-                contenido+='<div class="col-12 col-sm-6 col-md-4 col-lg-4 col-xl-4 col-xxl-4 image-overflow"><img style="width: 28rem;height: 30rem;" title="'+titulo+'" src="'+feed.media_url+'" onclick="window.open(\''+feed.permalink+'\');"></div>';
+                contenido+='<div class="col-12 col-sm-6 col-md-4 col-lg-4 col-xl-4 col-xxl-4 image-overflow"><img style="width: 26rem;height: 28rem;" title="'+titulo+'" src="'+feed.media_url+'" onclick="window.open(\''+feed.permalink+'\');"><div class="opacity-hover"></div></div>';
             }
         }
         contenido+='</div>';
         $('#insta').html(contenido);
     })
-})
+})*/
 
 
 
@@ -65,3 +134,4 @@ function moveGallery(e) {
     feed.scrollLeft -= feed.offsetWidth;
   }
 }*/
+
